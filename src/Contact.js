803b964438700f08e3de1a00f3ebdfffc73dda03
SaulEspinoza7 <ciberelmas4@gmail.com>
2023-09-16 23:43:56 -0400

    Tenth Sol Commit

--- src/Contact.js
+++ src/Contact.js
@@ -1,6 +1,9 @@
 import NavBar from "./NavBar";
-import "./styles/Contact.css"
-import Photo1 from "./images/photo1.jpeg"
+import "./styles/Contact.css";
+import Photo1 from "./images/photo1.jpeg";
+import Photo2 from "./images/photo2.jpeg";
+import Photo3 from "./images/photo3.jpeg";
+import Photo4 from "./images/photo4.jpeg";
 
 const Contact = () => {
     return (
@@ -14,24 +17,28 @@ const Contact = () => {
 
             <div className="team">
                 <div className="member">
+                    <img src={Photo1}></img>
                     <h3>Nawfal Cherkaoui Elmalki</h3>
                     <p>Github</p>
                     <p>LinkedIn</p>
                 </div>
 
                 <div className="member">
-                    <h3>Santiago </h3>
-                    <p>Github</p>
+                    <img src={Photo2}></img>
+                    <h3>Santiago Acosta</h3>
+                    <a href="https://github.com/santiago1595/">Github</a>
                     <p>LinkedIn</p>
                 </div>
 
                 <div className="member">
+                    <img src={Photo3}></img>
                     <h3>Saul Espinoza Nalvarte</h3>
-                    <p>Github</p>
+                    <a href="https://github.com/SaulEspinoza7">Github</a>
                     <p>LinkedIn</p>
                 </div>
 
                 <div className="member">
+                    <img src={Photo4}></img>
                     <h3>George Doujaiji</h3>
                     <p>Github</p>
                     <p>LinkedIn</p>
