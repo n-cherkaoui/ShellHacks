a991ac3c3d2eb12fd73d8f1526d1179634708859
SaulEspinoza7 <ciberelmas4@gmail.com>
2023-09-16 21:40:45 -0400

    Seventh?

--- src/apis/stocks.py
+++ src/apis/stocks.py
@@ -4,7 +4,11 @@ import json
 app = FastAPI()
 
 # Load data from a JSON file (replace 'your_data.json' with your actual JSON file)
+<<<<<<< HEAD
 with open('your_data.json', 'r') as json_file:
+=======
+with open('hardcode.json', 'r') as json_file:
+>>>>>>> 39837517 (api update)
     data = json.load(json_file)
 
 @app.get("/stocks")
